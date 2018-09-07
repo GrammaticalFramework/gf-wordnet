a129c5e8c77f1273ccd644b8aea410ea69b4a562
Krasimir Angelov <kr.angelov@gmail.com>
2018-09-07 09:22:44 +0200

    added a draft for a web UI

---
+++ www/js/gfwordnet.js
@@ -0,0 +1,36 @@
+gfwordnet = {}
+
+gfwordnet.grammar_url = "http://cloud.grammaticalframework.org/robust/Parse.pgf"
+
+gfwordnet.grammar_call=function(querystring,cont,errcont) {
+    http_get_json(gfwordnet.grammar_url+querystring,cont,errcont)
+}
+
+gfwordnet.initialize = function () {
+}
+
+gfwordnet.search = function (from, input, result) {
+	function errcont(text,code) { }
+	function extract_linearization(lins) {
+		var indices = {"ParseBul": 1, "ParseEng": 2, "ParseSwe": 3};
+		for (var i in lins) {
+			var lin = lins[i];
+			this[indices[lin.to]].appendChild(text(lin.text));
+		}
+	}
+	function extract_search(lemmas) {
+		clear(result);
+		var index = 1;
+		result.appendChild(tr([th(text("Abstract")),th(text("Bulgarian")),th(text("English")),th(text("Swedish"))]));
+		for (var i in lemmas) {
+			var row = [td(text(lemmas[i].lemma)),td([]),td([]),td([])];
+
+			result.appendChild(tr(node("td",{colspan: 4},[text(index+". gloss")]))); index++;
+			result.appendChild(tr(row));
+
+			gfwordnet.grammar_call("?command=c-linearize&to=ParseBul%20ParseEng%20ParseSwe&tree="+encodeURIComponent(lemmas[i].lemma),bind(extract_linearization,row),errcont);
+		}
+    }
+
+	gfwordnet.grammar_call("?command=c-lookupmorpho&input="+encodeURIComponent(input)+"&from="+from,extract_search,errcont);
+}

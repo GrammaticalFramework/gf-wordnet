2ac4571e4c380b423817b14866bf8767183f8f37
Krasimir Angelov <kr.angelov@gmail.com>
2018-09-07 13:58:16 +0200

    start using FCGI

--- sense-service/SenseService.hs
+++ sense-service/SenseService.hs
@@ -1,34 +1,76 @@
+{-# LANGUAGE CPP #-}
 import SG
 import PGF2
 import SenseSchema
 import qualified Data.Map as Map
 import Control.Monad(foldM)
+import Control.Concurrent(forkIO)
+import Network.CGI
+import Network.FastCGI(runFastCGI,runFastCGIConcurrent')
+import qualified Codec.Binary.UTF8.String as UTF8 (encodeString)
 import Text.JSON
 
 main = do
   db <- openSG "semantics.db"
-  let lex_ids = [lex_id | Just lex_id <- [readExpr "apple_1_N", readExpr "book_1_N"]]
-  senses <- foldM (getSense db) Map.empty lex_ids
-  putStrLn (encode (showJSON (map mkSenseObj (Map.toList senses))))
+#ifndef mingw32_HOST_OS
+  runFastCGIConcurrent' forkIO 100 (cgiMain db)
+#else
+  runFastCGI (cgiMain db)
+#endif
   closeSG db
+
+
+cgiMain :: SG -> CGI CGIResult
+cgiMain db = do
+  json <- liftIO doQuery
+  r <- outputJSONP json
+  return r
   where
-    mkSenseObj (sense_id,(mb_gloss,domains,lex_ids)) =
-      makeObj ([("sense_id",showJSON sense_id)]++
-               maybe [] (\gloss->[("gloss",showJSON gloss)]) mb_gloss++
-               [("lex_ids",showJSON (map (showExpr []) lex_ids))]++
-               [("domains",showJSON (map (showExpr []) domains))])
-
-    getSense db senses lex_id = do
-      res <- queryTriple db (Just lex_id) (Just synset) Nothing
-      foldM (getGloss db) senses res
-
-    getGloss db senses x@(_,lex_id,_,sense_id_e) = 
-      case Map.lookup sense_id senses of
-        Just (mb_gloss,domains,lex_ids) -> return (Map.insert sense_id (mb_gloss,domains,lex_id:lex_ids) senses)
-        Nothing                         -> do res <- queryTriple db (Just sense_id_e) (Just gloss) Nothing
-                                              let mb_gloss = head ([unStr gloss_str | (_,_,_,gloss_str) <- res]++[Nothing])
-                                              res <- queryTriple db (Just sense_id_e) (Just domain) Nothing
-                                              let domains = [domain_str | (_,_,_,domain_str) <- res]
-                                              return (Map.insert sense_id (mb_gloss,domains,[lex_id]) senses)
+    doQuery = do
+      let lex_ids = [lex_id | Just lex_id <- [readExpr "apple_1_N", readExpr "book_1_N"]]
+      senses <- foldM (getSense db) Map.empty lex_ids
+      putStrLn (encode (showJSON (map mkSenseObj (Map.toList senses))))
       where
-        Just (sense_id,[]) = unApp sense_id_e
+        mkSenseObj (sense_id,(mb_gloss,domains,lex_ids)) =
+          makeObj ([("sense_id",showJSON sense_id)]++
+                   maybe [] (\gloss->[("gloss",showJSON gloss)]) mb_gloss++
+                   [("lex_ids",showJSON (map (showExpr []) lex_ids))]++
+                   [("domains",showJSON (map (showExpr []) domains))])
+
+        getSense db senses lex_id = do
+          res <- queryTriple db (Just lex_id) (Just synset) Nothing
+          foldM (getGloss db) senses res
+
+        getGloss db senses x@(_,lex_id,_,sense_id_e) = 
+          case Map.lookup sense_id senses of
+            Just (mb_gloss,domains,lex_ids) -> return (Map.insert sense_id (mb_gloss,domains,lex_id:lex_ids) senses)
+            Nothing                         -> do res <- queryTriple db (Just sense_id_e) (Just gloss) Nothing
+                                                  let mb_gloss = head ([unStr gloss_str | (_,_,_,gloss_str) <- res]++[Nothing])
+                                                  res <- queryTriple db (Just sense_id_e) (Just domain) Nothing
+                                                  let domains = [domain_str | (_,_,_,domain_str) <- res]
+                                                  return (Map.insert sense_id (mb_gloss,domains,[lex_id]) senses)
+          where
+            Just (sense_id,[]) = unApp sense_id_e
+
+
+outputJSONP :: JSON a => a -> CGI CGIResult
+outputJSONP = outputEncodedJSONP . encode
+
+outputEncodedJSONP :: String -> CGI CGIResult
+outputEncodedJSONP json = 
+    do mc <- getInput "jsonp"
+       let (ty,str) = case mc of
+                        Nothing -> ("json",json)
+                        Just c  -> ("javascript",c ++ "(" ++ json ++ ")")
+           ct = "application/"++ty++"; charset=utf-8"
+       outputText ct str
+
+outputText ct = outputStrict ct . UTF8.encodeString
+
+outputStrict :: String -> String -> CGI CGIResult
+outputStrict ct x | x == x = do setHeader "Content-Type" ct
+                                setXO
+                                output x
+                  | otherwise = fail "I am the pope."
+
+setXO = setHeader "Access-Control-Allow-Origin" "*"

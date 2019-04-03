61ea4fd1fcb8a28571d888c09e8fcc9a4ec74cca
krangelov <kr.angelov@gmail.com>
2019-04-03 08:42:33 +0200

    apply checks per language

--- check.hs
+++ check.hs
@@ -1,39 +1,42 @@
 import Data.Maybe
 import Data.List
 import Data.Char
+import qualified Data.Map as Map
 import PGF2
+import System.IO
 import System.Environment
 import System.Directory
+import System.FilePath
+import Control.Monad(forM_)
 
 main = do
   args    <- getArgs
   checked <- case args of
-               []         -> do es <- fmap (catMaybes . map (readExpr . drop 5) . filter (\l -> take 4 l == "abs:") . lines) $ readFile "examples.txt"
-                                return (nub (concatMap lemmas es))
-               ("-":args) -> return args
-  if null checked
-    then return ()
-    else do ls <- fmap lines $ readFile "WordNet.gf"
-            writeFile "WordNet2.gf" (unlines (map (annotate checked) ls))
-            renameFile "WordNet2.gf" "WordNet.gf"
-
-lemmas e =
-  case unApp e of
-    Just (f,[]) -> [f]
-    Just (f,es) -> concatMap lemmas es
-    _           -> []
-  
-annotate checked l =
-  case break (=='\t') l of
-    (xs,'\t':'[':ys) -> case break (==']') ys of
-                          (ys,']':zs) -> let domains = map strip (lines (map (\c -> if c == ',' then '\n' else c) ys))
-                                             new_domains = if elem id checked then delete "unchecked" domains else domains
-                                         in if null new_domains
-                                              then xs++"\t"++dropWhile isSpace zs
-                                              else xs++"\t["++intercalate ", " new_domains++"]"++zs
-                          _          -> l
-    _                                -> l
+               []    -> do es <- fmap (catMaybes . map (readExpr . drop 5) . filter (\l -> take 4 l == "abs:") . lines) $ readFile "examples.txt"
+                           let ids = nub (concatMap lemmas es)
+                           return [("ParseEng",ids),("ParseSwe",ids),("ParseBul",ids)]
+               ["-"] -> do ls <- fmap lines $ getContents
+                           let res = [(lang,[(id,Just (unwords ws))]) | l <- ls, let (id:lang:ws) = words l]
+                           return ((Map.toList . Map.fromListWith (++)) res) 
+  forM_ checked $ \(lang,ids) ->
+    if null ids
+      then return ()
+      else do let fname = "WordNet"++drop 5 lang++".gf"
+              ls <- fmap lines $ readFile fname
+              (tmp_fname,hTmp) <- openTempFile "." fname
+              mapM_ (hPutStrLn hTmp . annotate ids) ls 
+              hClose hTmp
+              renameFile tmp_fname fname
   where
-    ("fun":id:_) = words l
+    lemmas e =
+      case unApp e of
+        Just (f,[]) -> [(f,Nothing)]
+        Just (f,es) -> concatMap lemmas es
+        _           -> []
 
-strip = reverse . dropWhile isSpace . reverse . dropWhile isSpace
+annotate checked l =
+  case words l of
+    ("lin":id:"=":_) -> case lookup id checked of
+                          Just def -> (reverse . dropWhile (/=';') . reverse) l
+                          Nothing  ->                                         l
+    _                              ->                                         l

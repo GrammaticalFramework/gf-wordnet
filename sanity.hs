eeb7fd7f411787c1a808623b4f0c98c59c86f4ed
krangelov <kr.angelov@gmail.com>
2020-03-28 15:51:55 +0100

    nor/neither conjuction

--- sanity.hs
+++ sanity.hs
@@ -9,7 +9,7 @@ main = do
   
 process gr []                     = []
 process gr (l0:l1:l2:l3:l4:l5:ls)
-  | take 4 l0 == "abs:"           = l0:(if l1m then l1 else "FIX: "++l1):(if l2m then l2 else "FIX: "++l2):(if l3m then l3 else "FIX: "++l3):l4:l5:process gr ls
+  | take 4 l0 == "abs:"           = l0:(if l1m then l1 else "FIX: "++l1++"\n          "++linearize eng e):(if l2m then l2 else "FIX: "++l2++"\n          "++linearize swe e):(if l3m then l3 else "FIX: "++l3++"\n          "++linearize bul e):l4:l5:process gr ls
   | take 8 l0 == "abs* Phr"       = (if l1m && l2m && l3m then "DONE: "++l0 else l0):l1:l2:l3:l4:l5:process gr ls
   | otherwise                     = l0:l1:l2:l3:l4:l5:process gr ls
   where

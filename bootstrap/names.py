719d1225b9050e9451303bdcf62b131d792f18cb
Krasimir Angelov <kr.angelov@gmail.com>
2022-08-01 12:02:09 +0200

    initial steps towards Bulgarian

--- bootstrap/names.py
+++ bootstrap/names.py
@@ -23,6 +23,118 @@ def extract():
                         names[lang] = val["value"]
                     out.write(str((record["id"],name_type,names))+"\n")
 
+def cyr(s):
+    #DOUBLE LETTERS UPPERCASE
+    s = s.replace("YU", "Ю")
+    s = s.replace("YA", "Я")
+    s = s.replace("ZH", "Ж")
+    s = s.replace("TS", "Ц")
+    s = s.replace("CH", "Ч")
+    s = s.replace("SHT", "Щ")
+    s = s.replace("SH", "Ш")
+    s = s.replace("QU", "КУ")
+    s = s.replace("FF", "Ф")
+    s = s.replace("KK", "К")
+    s = s.replace("OO", "О")
+    #DOUBLE LETTERS MIXED CASE
+    s = s.replace("Yu", "Ю")
+    s = s.replace("Ya", "Я")
+    s = s.replace("Zh", "Ж")
+    s = s.replace("Ts", "Ц")
+    s = s.replace("Ch", "Ч")
+    s = s.replace("Sht", "Щ")
+    s = s.replace("Sh", "Ш")
+    s = s.replace("Qf", "Ку")
+    s = s.replace("Ff", "Ф")
+    s = s.replace("Kk", "К")
+    s = s.replace("Oo", "О")
+    #DOUBLE LETTERS LOWERCASE
+    s = s.replace("yu", "ю")
+    s = s.replace("ya", "я")
+    s = s.replace("zh", "ж")
+    s = s.replace("ts", "ц")
+    s = s.replace("ch", "ч")
+    s = s.replace("sht", "щ")
+    s = s.replace("sh", "ш")
+    s = s.replace("qu", "ку")
+    s = s.replace("ff", "ф")
+    s = s.replace("kk", "k")
+    s = s.replace("oo", "о")
+    #UPPERCASE LETTERS
+    s = s.replace("A", "A")
+    s = s.replace("B", "Б")
+    s = s.replace("C", "К")
+    s = s.replace("V", "В")
+    s = s.replace("G", "Г")
+    s = s.replace("D", "Д")
+    s = s.replace("E", "E")
+    s = s.replace("Z", "З")
+    s = s.replace("I", "И")
+    s = s.replace("J", "Й")
+    s = s.replace("K", "К")
+    s = s.replace("L", "Л")
+    s = s.replace("M", "M")
+    s = s.replace("N", "Н")
+    s = s.replace("O", "O")
+    s = s.replace("P", "П")
+    s = s.replace("R", "Р")
+    s = s.replace("S", "С")
+    s = s.replace("T", "T")
+    s = s.replace("Ț", "T")
+    s = s.replace("W", "В")
+    s = s.replace("F", "Ф")
+    s = s.replace("H", "X")
+    s = s.replace("U", "У")
+    s = s.replace("Y", "Й")
+    s = s.replace("Æ", "Е")
+    s = s.replace("Ä", "Е")
+    s = s.replace("É", "Е")
+    s = s.replace("Ə", "А")
+    s = s.replace("Î", "И")
+    s = s.replace("Ö", "Йо")
+    s = s.replace("Ø", "Йо")
+    s = s.replace("Õ", "Йо")
+    s = s.replace("Ó", "О")
+    s = s.replace("Å", "О")
+    #LOWERCASE LETTERS
+    s = s.replace("a", "а")
+    s = s.replace("b", "б")
+    s = s.replace("c", "к")
+    s = s.replace("v", "в")
+    s = s.replace("g", "г")
+    s = s.replace("d", "д")
+    s = s.replace("e", "е")
+    s = s.replace("z", "з")
+    s = s.replace("i", "и")
+    s = s.replace("j", "й")
+    s = s.replace("k", "к")
+    s = s.replace("l", "л")
+    s = s.replace("m", "м")
+    s = s.replace("n", "н")
+    s = s.replace("o", "о")
+    s = s.replace("p", "п")
+    s = s.replace("r", "р")
+    s = s.replace("s", "с")
+    s = s.replace("t", "т")
+    s = s.replace("ț", "т")
+    s = s.replace("w", "в")
+    s = s.replace("f", "ф")
+    s = s.replace("h", "х")
+    s = s.replace("u", "у")
+    s = s.replace("y", "и")
+    s = s.replace("æ", "е")
+    s = s.replace("ä", "е")
+    s = s.replace("é", "е")
+    s = s.replace("ə", "а")
+    s = s.replace("î", "и")
+    s = s.replace("ö", "йо")
+    s = s.replace("ø", "йо")
+    s = s.replace("õ", "йо")
+    s = s.replace("ó", "о")
+    s = s.replace("å", "о")
+    
+    return s;
+
 def generate():
     with open("names.txt", "r") as f:
         gf_ids = {}
@@ -84,7 +196,7 @@ def generate():
 
         langs = [
           ("Afr",["af","nl","en"]),
-          ("Bul",["bg","sr","ru"]),
+          ("Bul",["bg","sr","ru","en"]),
           ("Cat",["ca","en"]),
           ("Chi",["zh"]),
           ("Dut",["nl","en"]),
@@ -110,7 +222,7 @@ def generate():
 
         for lang,lang_codes  in langs:
             with open("Names"+lang+".gf","w") as out:
-                out.write("concrete Names"+lang+" of Names = open Cat"+lang+", Paradigms"+lang+" in {\n")
+                out.write("concrete Names"+lang+" of Names = Cat"+lang+" ** open Paradigms"+lang+" in {\n")
                 out.write("\n")
                 out.write("lincat GN, SN = PN ;\n")
                 out.write("\n")
@@ -120,16 +232,24 @@ def generate():
                     for lang_code in lang_codes:
                         s = labels.get(lang_code)
                         if s:
+                            if lang == "Bul" and lang_code != "bg":
+                                s  = cyr(s)
                             lin_list = s.split("/")
                             break
                     for lin in lin_list:
                         lin = lin.strip()
-                        if name_type in ["Q12308941","Q18972245"]:
-                            lin = "regGenPN \""+lin+"\" masculine"
-                        elif name_type in ["Q11879590","Q18972207"]:
-                            lin = "regGenPN \""+lin+"\" feminine"
-                        else:
-                            lin = "mkPN \""+lin+"\""
+                        if lang == "Eng":
+                            if name_type in ["Q12308941","Q18972245"]:
+                                lin = "regGenPN \""+lin+"\" masculine"
+                            elif name_type in ["Q11879590","Q18972207"]:
+                                lin = "regGenPN \""+lin+"\" feminine"
+                            else:
+                                lin = "mkPN \""+lin+"\""
+                        elif lang == "Bul":
+                            if name_type in ["Q11879590","Q18972207"]:
+                                lin = "mkPN \""+lin+"\" Fem"
+                            else:
+                                lin = "mkPN \""+lin+"\" Masc"
                         lins.append(lin)
                     if len(lins) == 1:
                         out.write("lin "+quote(gf_id)+" = "+lins[0]+" ;\n")

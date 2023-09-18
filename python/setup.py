ec849ee7497ad5f1c80038fca13ffcc5d62014a9
Krasimir Angelov <kr.angelov@gmail.com>
2023-09-18 13:41:43 +0200

    fix the dependency

--- python/setup.py
+++ python/setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name = "gf-wordnet",
-    version = "0.0.9",
+    version = "0.0.10",
     author = "Krasimir Angelov",
     author_email = "kr.angelov@gmail.com",
     description = ("An API to the GF WordNet."),
@@ -14,7 +14,7 @@ setup(
     keywords = "GF WordNet",
     url = "https://github.com/GrammaticalFramework/gf-wordnet",
     packages=['wordnet'],
-    install_requires=['pgf','daison'],
+    install_requires=['pgf-majestic','daison'],
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
     zip_safe=False

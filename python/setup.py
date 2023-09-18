729dfc5e26c61c62b7826714071774bcd5966ed0
Krasimir Angelov <kr.angelov@gmail.com>
2023-09-18 13:30:02 +0200

    quick fix for downloading several languages

--- python/setup.py
+++ python/setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name = "gf-wordnet",
-    version = "0.0.7",
+    version = "0.0.8",
     author = "Krasimir Angelov",
     author_email = "kr.angelov@gmail.com",
     description = ("An API to the GF WordNet."),

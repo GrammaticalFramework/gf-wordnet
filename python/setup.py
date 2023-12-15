527ad9241e81a0c33361ae52d3a35f0833df34a9
Krasimir Angelov <kr.angelov@gmail.com>
2023-12-15 12:44:46 +0100

    specify the encoding for README.md to utf8 to make the package install on Windows

--- python/setup.py
+++ python/setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 def read(fname):
     import os
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    return open(os.path.join(os.path.dirname(__file__), fname),encoding="utf8").read()
 
 setup(
     name = "gf-wordnet",
-    version = "0.0.11",
+    version = "0.0.12",
     author = "Krasimir Angelov",
     author_email = "kr.angelov@gmail.com",
     description = ("An API to the GF WordNet."),
